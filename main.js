4b0c6f50ec97417c3a0d16a37a608cefbc645aa0
unknown <kylelarsen12@gmail.com>
2022-06-04 13:07:22 -0400

    Add user location functionality & marker

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-//Leaflet stuff
+/*Leaflet stuff
 var map = L.map("map").setView([51.505, -0.09], 13);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
+*/
 
 /*
 -Need code for:
@@ -15,4 +16,47 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         -Add popups when businesses are hovered over/clicked
 
         api key: fsq3XcDK8dkjWj4yZXVUYXClvuc0vwuw9oaPk6X3rbyf0/I=
+
+
 */
+
+//create map object
+let myMap = {
+  coords: [],
+  localBusinesses: [],
+  map: {},
+  markers: {},
+
+  buildMap() {
+    this.map = L.map("map", {
+      center: this.coords,
+      zoom: 14,
+    });
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution: "© OpenStreetMap",
+    }).addTo(this.map);
+
+    let userMarker = L.marker(this.coords);
+    userMarker.addTo(this.map).bindPopup("<p>This is you</p>").openPopup();
+  },
+};
+
+//Get user location
+async function getCoords() {
+  const pos = await new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+  return [pos.coords.latitude, pos.coords.longitude];
+}
+
+//Build map
+window.onload = async () => {
+  let userCoords = await getCoords();
+  myMap.coords = userCoords;
+  myMap.buildMap();
+};
+
+//Get user selection for business type and add markers
+let userChoice = document.getElementById("options").value;
+console.log(userChoice);
